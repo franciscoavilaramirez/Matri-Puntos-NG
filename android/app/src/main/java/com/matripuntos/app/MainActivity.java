f71647c4fec6a11c30715b3dd96d67a26b886a47
Francisco Avila <fran_loja10@hotmail.com>
2026-08-31 12:56:36 +0200

    Convirtiendo web en app android

---
+++ android/app/src/main/java/com/matripuntos/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.matripuntos.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.matripuntos.app',
+  appName: 'MatriPuntos',
+  webDir: 'dist/matripuntos-ng/browser'
+};
+
+export default config;
